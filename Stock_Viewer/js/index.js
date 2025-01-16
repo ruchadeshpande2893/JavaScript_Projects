2868ff5017a0872373cf625e742ff12af7c1645d
ruchadeshpande2893 <ruchadeshpande2893@gmail.com>
2025-01-15 16:03:47 -0800

    Add Stock_Viewer

---
+++ Stock_Viewer/js/index.js
@@ -0,0 +1,29 @@
+document.getElementById("submit-button").addEventListener("click", function() {
+    const symbol = document.getElementById("ticker-symbol").value;
+    const request = new XMLHttpRequest();
+    const api = "http://api.marketstack.com/v1/eod/latest?access_key=87af132a4335fbc7720bd4af398274d4&symbols=" + symbol;
+    request.open("GET", api, false);
+    request.send();
+    const responseJson = JSON.parse(request.responseText);
+
+    if(responseJson.hasOwnProperty("error")) {
+        alert(responseJson.error.message);
+    } else {
+        const open = responseJson.data[0].open;
+        const high = responseJson.data[0].high;
+        const low = responseJson.data[0].low;
+        const companySymbol = responseJson.data[0].symbol;
+        const exchange = responseJson.data[0].exchange;
+        const date = responseJson.data[0].date;
+        document.getElementById("open-p").innerHTML = open;
+        document.getElementById("high-p").innerHTML = high;
+        document.getElementById("low-p").innerHTML = low;
+        document.getElementById("symbol-p").innerHTML = companySymbol;
+        document.getElementById("exchange-p").innerHTML = exchange;
+        document.getElementById("date-p").innerHTML = date.slice(0,10);
+    }
+    
+    document.getElementById("ticker-symbol").value = "";
+})
+
+
